568c2bcbcfae0536feb6b4c198fa1e09cd13cbb9
ethxn-frs <ethanfrancois0@gmail.com>
2023-03-08 19:31:43 +0100

    [User] feature/ started making crud

--- templates/includes/headerBackoffice.php
+++ templates/includes/headerBackoffice.php
@@ -4,7 +4,10 @@
       <div class="d-flex">
         <img src="{{ asset('assets/logo.jpg') }}" alt="">
         <nav class="d-flex white">
-          <a href="" class="{% if app.request.get('_route') == 'foo_products_overview' %} class="active"{% endif %}">Accueil</a>
+          <a href="/backoffice">Accueil</a>
+          <a href="">Partenariat</a>
+          <a href="">A propos de nous</a>
+          <a href="/user">Gestion Administrateurs</a>
           <a href="/logout">Déconnexion</a>
         </nav>
       </div>
