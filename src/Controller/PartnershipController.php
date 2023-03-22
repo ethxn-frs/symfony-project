b43d747f79cc0dc8e33ba67dbce9ba6c6a9912bf
qrulleau <quentin.rulleau@yahoo.com>
2023-03-22 12:14:31 +0100

    ADD: dynamic content for partner page

--- src/Controller/PartnershipController.php
+++ src/Controller/PartnershipController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Partnership;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PartnershipRepository;
 
 
 class PartnershipController extends AbstractController
 {
     #[Route('/partnership', name: 'partenaire')]
-    public function index(): Response
+    public function index(PartnershipRepository $partnerRepository): Response
     {
         return $this->render('partnership/index.html.twig', [
             'controller_name' => 'PartnershipController',
+            'partners' => $partnerRepository->findAll()
         ]);
     }
 }
